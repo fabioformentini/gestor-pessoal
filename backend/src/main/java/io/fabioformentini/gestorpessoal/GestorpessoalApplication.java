6bebb6da28d7518db21a457e98b508db7e307c67
fabioformentini <fbformentini@gmail.com>
2022-03-30 13:31:16 -0300

    Adicionando diretórios frontend e backend da aplicação

---
+++ backend/src/main/java/io/fabioformentini/gestorpessoal/GestorpessoalApplication.java
@@ -0,0 +1,13 @@
+package io.fabioformentini.gestorpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorpessoalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/fabioformentini/gestorpessoal/GestorpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fabioformentini.gestorpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
